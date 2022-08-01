f2dca9d70040a0e49a05f4ac920fb329798f494a
Abbas Attarwala <1814309+abbasdgr8@users.noreply.github.com>
2022-08-02 00:49:20 +1000

    init: Added basic project structure for Java11

---
+++ java11-boot3-h2/src/main/java/com/examples/abbasdgr8/Java11Boot3H2Application.java
@@ -0,0 +1,13 @@
+package com.examples.abbasdgr8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11Boot3H2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11Boot3H2Application.class, args);
+	}
+
+}
---
+++ java11-boot3-h2/src/test/java/com/examples/abbasdgr8/Java11Boot3H2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.abbasdgr8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11Boot3H2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
