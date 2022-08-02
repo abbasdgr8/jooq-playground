843bc855d807429d2dfa5b5718a8e76435cdb152
Abbas Attarwala <1814309+abbasdgr8@users.noreply.github.com>
2022-08-03 00:40:42 +1000

    config: Bumped down Spring version
    Brought down Spring Boot Starter version from 3.0.0-M4 to 2.7.2 to
    support compatibility with Java 8

--- java8-boot272-h2/src/main/java/com/examples/abbasdgr8/Java8Boot272H2Application.java
+++ java8-boot272-h2/src/main/java/com/examples/abbasdgr8/Java8Boot272H2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java8Boot3H2Application {
+public class Java8Boot272H2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Java8Boot3H2Application.class, args);
+		SpringApplication.run(Java8Boot272H2Application.class, args);
 	}
 
 }
