686f7d9162a8d40f8dd801c9ed90c0718a5e8bb9
Abbas Attarwala <1814309+abbasdgr8@users.noreply.github.com>
2022-08-04 19:53:25 +1000

    rename: Changed Renamed all class file names

--- java8-boot272-h2/src/test/java/com/examples/abbasdgr8/InMemoryH2DatabaseTests.java
+++ java8-boot272-h2/src/test/java/com/examples/abbasdgr8/InMemoryH2DatabaseTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Java8Boot272H2ApplicationTests {
+class InMemoryH2DatabaseTests {
 
 	@Test
 	void contextLoads() {
