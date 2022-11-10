d9b4228ac5f676d08c9d6b84042a99aac70c006d
Abbas Attarwala <1814309+abbasdgr8@users.noreply.github.com>
2022-11-10 17:35:33 +1000

    style: Fixed order of imports

--- h2-db-server/src/main/java/com/examples/abbasdgr8/repositories/CountryRepository.java
+++ h2-db-server/src/main/java/com/examples/abbasdgr8/repositories/CountryRepository.java
@@ -1,8 +1,11 @@
 package com.examples.abbasdgr8.repositories;
 
-import com.examples.abbasdgr8.models.Country;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.examples.abbasdgr8.models.Country;
+
 
 /**
  * @author Abbas Attarwala <abbas@muon-c.com>
